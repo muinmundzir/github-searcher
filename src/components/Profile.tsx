bc7994d2d0c0112e0fcb16d109f5a560b7d50133
Cloud <muin@docheck.id>
2022-12-25 15:29:39 +0700

    feat: add navigation

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,49 +1,49 @@
 interface ProfileProps {}
 
-const Profile = ({ users }) => {
+const Profile = ({ user }) => {
   return (
     <article className="max-w-sm mx-auto rounded-md shadow-md bg-gray-200 mb-4">
       <div className="p-4 pb-0 mb-4 flex gap-4">
         <img
-          className="h-20 w-20 rounded-full"
-          src={users?.avatar_url!}
+          className="h-20 w-20 rounded-md"
+          src={user?.avatar_url!}
           alt="users avatar"
         />
         <div>
-          <h2 className="text-normal">@{users.login!}</h2>
-          <p className="text-sm">{users.bio}</p>
+          <h2 className="text-normal">@{user.login!}</h2>
+          <p className="text-sm">{user.bio}</p>
         </div>
       </div>
       <section className="p-4 flex items-center justify-between border-t-2 border-t-black">
         <div className="flex flex-col items-center justify-center">
           <a
-            href={`https://api.github.com/users/${users.login}/following`}
+            href={`https://api.github.com/users/${user.login}/following`}
             className="text-sm"
           >
             Following
           </a>
-          <p className="text-sm">{users.following}</p>
+          <p className="text-sm">{user.following}</p>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <a href={users.followers_url} className="text-sm">
+          <a href={user.followers_url} className="text-sm">
             Followers
           </a>
-          <p className="text-sm">{users.followers}</p>
+          <p className="text-sm">{user.followers}</p>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <a href={users.repos_url} className="text-sm">
+          <a href={user.repos_url} className="text-sm">
             Repos
           </a>
-          <p className="text-sm">{users.public_repos}</p>
+          <p className="text-sm">{user.public_repos}</p>
         </div>{" "}
         <div className="flex flex-col items-center justify-center">
           <a
-            href={`https://api.github.com/users/${users.login}/gists`}
+            href={`https://api.github.com/users/${user.login}/gists`}
             className="text-sm"
           >
             Gists
           </a>
-          <p className="text-sm">{users.public_gists}</p>
+          <p className="text-sm">{user.public_gists}</p>
         </div>
       </section>
     </article>
